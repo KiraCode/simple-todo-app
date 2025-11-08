0391dfce2137e5dfab8bc26c662453b0c214acd4
tejas git <d9489079@gmail.com>
2025-11-08 19:47:10 +0530

    adding router endpoints for crud operation

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const router = express.Router();
+// router.post("/task" newTask);
+// router.get("/tasks", getTasks);
+// router.put("/task/:id", updateTask);
+// router.delete("/task/:id", deleteTask);
+
+export default router;
