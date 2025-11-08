da4d5560e6a550843f16ab6a5406e0ce43308ccd
tejas git <d9489079@gmail.com>
2025-11-08 19:08:56 +0530

    added backend node setupt for the todo app project

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.port || 8001;
+
+app.use(cors());
+app.use(express.urlencoded({extended:true})); //parse the post request coming from request body
+app.use(express.json()); // accept the json data from frontend
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...😉`);
+});
